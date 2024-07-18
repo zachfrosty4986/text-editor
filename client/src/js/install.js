62a48f7aa9013bbce0c5887941b4632591a2e217
zachfrosty4986 <zachjames0114@gmail.com>
2024-07-17 20:56:14 -0400

    Added handlers to install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,33 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt;
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// TODO: Add an event handler to the beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  butInstall.style.display = 'block'; // Show install button
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// TODO: Implement a click event handler on the butInstall element
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null; // Reset the deferredPrompt
+      butInstall.style.display = 'none'; // Hide install button after installation
+    });
+  }
+});
+
+// TODO: Add an handler for the appinstalled event
+window.addEventListener('appinstalled', (event) => {
+  console.log('App was installed');
+});
